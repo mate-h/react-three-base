ddc671233a6c90ee74605766c0752fac6e774ca0
Máté Homolya <mate.homolya@gmail.com>
2023-10-24 17:31:18 -0700

    Update to Leva

--- src/box.tsx
+++ src/box.tsx
@@ -1,16 +1,30 @@
 import { useFrame } from '@react-three/fiber'
 import frag from './box.frag'
-import { params } from './pane'
+import { useControls } from 'leva'
+import { useEffect } from 'react'
+import { Uniform } from 'three'
+
+const Control = ({ speed }: { speed: Uniform }) => {
+  const { speed: speedControl } = useControls({
+    speed: { value: 0.5, min: 0, max: 1 },
+  })
+  useEffect(() => {
+    speed.value = speedControl
+  }, [speedControl])
+  return null
+}
 
 export default () => {
-  const time = { value: 0 }
-  const speed = { value: params.speed }
+  const time = new Uniform(0)
+  const speed = new Uniform(0.5)
+
   useFrame((state) => {
     time.value = state.clock.getElapsedTime()
-    speed.value = params.speed
   })
+
   return (
     <mesh>
+      <Control speed={speed} />
       <boxGeometry />
       <shaderMaterial fragmentShader={frag} uniforms={{ time, speed }} />
     </mesh>
